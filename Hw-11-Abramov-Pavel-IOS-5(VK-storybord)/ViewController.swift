9701cc6ae7ac1a0a7bdd35a6b7d67041a1a6c586
PavelAbramoff <96954222+PavelAbramoff@users.noreply.github.com>
2022-04-25 09:18:43 +0400

    Initial Commit

---
+++ Hw-11-Abramov-Pavel-IOS-5(VK-storybord)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hw-11-Abramov-Pavel-IOS-5(VK-storybord)
+//
+//  Created by Pavel Абрамов on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
